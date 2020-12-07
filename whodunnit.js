02134756234d8c32120d590435a2e2c36a1de965
Michalju <juszkin@gmail.com>
2020-12-07 16:18:49 +0000

    Episode 2 done

--- whodunnit.js
+++ whodunnit.js
@@ -1,18 +1,32 @@
 //Epsiode 1:
-const scenario = {
-    murderer: 'Miss Scarlet',
-    room: 'Library',
-    weapon: 'Rope'
-  };
-  
-  const declareMurderer = function() {
-    return `The murderer is ${scenario.murderer}.`;
-  }
-  
-  const verdict = declareMurderer();
-  console.log(verdict);
+// const scenario = {
+//     murderer: 'Miss Scarlet',
+//     room: 'Library',
+//     weapon: 'Rope'
+//   };
+
+//   const declareMurderer = function() {
+//     return `The murderer is ${scenario.murderer}.`;
+//   }
+
+//   const verdict = declareMurderer();
+//   console.log(verdict);
 
 //Prediction: Miss Scarlet
 
+//Epsiode 2:
+// const murderer = 'Professor Plum';
 
-// Comment all above beofre moving to a new episode
+// const changeMurderer = function () {
+//     murderer = 'Mrs. Peacock';
+// }
+
+// const declareMurderer = function () {
+//     return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+//Prediction: thought Professor Plum will be returned but it got error which is correct
