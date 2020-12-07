344cbda1ef63d5b386510213ad7331f6c13e9219
Michalju <juszkin@gmail.com>
2020-12-07 17:00:00 +0000

    Episode 8 done

--- whodunnit.js
+++ whodunnit.js
@@ -112,30 +112,68 @@
 //#####################################################################################
 
 //Epsiode 7:
-let murderer = 'Professor Plum';
+// let murderer = 'Professor Plum';
 
-const changeMurderer = function() {
-  murderer = 'Mr. Green';
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
 
-  const plotTwist = function() {
-    let murderer = 'Colonel Mustard';
+//   const plotTwist = function() {
+//     let murderer = 'Colonel Mustard';
 
-    const unexpectedOutcome = function() {
-      murderer = 'Miss Scarlet';
-    }
+//     const unexpectedOutcome = function() {
+//       murderer = 'Miss Scarlet';
+//     }
 
-    unexpectedOutcome();
-  }
+//     unexpectedOutcome();
+//   }
 
-  plotTwist();
-}
+//   plotTwist();
+// }
 
-const declareMurderer = function() {
-  return `The murderer is ${murderer}.`;
-}
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
 
-changeMurderer();
-const verdict = declareMurderer();
-console.log(verdict);
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
 //Prediction: Miss Scarlet but it makes sense why Mr. Green
+//#####################################################################################
+
+//Epsiode 8:
+const scenario = {
+    murderer: 'Mrs. Peacock',
+    room: 'Conservatory',
+    weapon: 'Lead Pipe'
+  };
+  
+  const changeScenario = function() {
+    scenario.murderer = 'Mrs. Peacock';
+    scenario.room = 'Dining Room';
+  
+    const plotTwist = function(room) {
+      if (scenario.room === room) {
+        scenario.murderer = 'Colonel Mustard';
+      }
+  
+      const unexpectedOutcome = function(murderer) {
+        if (scenario.murderer === murderer) {
+          scenario.weapon = 'Candle Stick';
+        }
+      }
+  
+      unexpectedOutcome('Colonel Mustard');
+    }
+  
+    plotTwist('Dining Room');
+  }
+  
+  const declareWeapon = function() {
+    return `The weapon is ${scenario.weapon}.`
+  }
+  
+  changeScenario();
+  const verdict = declareWeapon();
+  console.log(verdict);
+//Prediction: Candle Stick
 //#####################################################################################
