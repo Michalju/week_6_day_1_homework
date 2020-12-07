af54d379894f087b1d9d7ec96d0c76a57fc91a46
Michalju <juszkin@gmail.com>
2020-12-07 16:09:49 +0000

    Episode 1 done

---
+++ whodunnit.js
@@ -0,0 +1,18 @@
+//Epsiode 1:
+const scenario = {
+    murderer: 'Miss Scarlet',
+    room: 'Library',
+    weapon: 'Rope'
+  };
+  
+  const declareMurderer = function() {
+    return `The murderer is ${scenario.murderer}.`;
+  }
+  
+  const verdict = declareMurderer();
+  console.log(verdict);
+
+//Prediction: Miss Scarlet
+
+
+// Comment all above beofre moving to a new episode
